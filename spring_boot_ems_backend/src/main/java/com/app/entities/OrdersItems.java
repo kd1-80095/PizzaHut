74ff0eb175164bda6a54202d77f8f069df6c39ed
kd1-80107 <prathameshnazare09@example.com>
2024-02-09 09:38:53 +0530

    Added: Orders, OrdersItem,Items (Entity)

---
+++ spring_boot_ems_backend/src/main/java/com/app/entities/OrdersItems.java
@@ -0,0 +1,43 @@
+package com.app.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Orders_Item")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+
+public class OrdersItems extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="orders_Items_Id",length=20)
+	private long ordersItemId;
+	//orderId --> fk
+	//itemsId --> fk
+	
+	@Column(name="Unit_Price",length=100)
+	private float unitPrice;
+	@Column(name="Quantity",length=100)
+	private int quantity;
+	@Column(name="Total_Amount",length=100)
+	private float totalAmount;
+	
+	
+}
